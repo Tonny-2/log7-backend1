8134689362a545d5e89c41d5bdcb2ed26552306d
sam <tonnymareba217@gmail.com>
2025-11-23 22:56:08 +0000

    bak1

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
 connectDB();
 app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port`);
