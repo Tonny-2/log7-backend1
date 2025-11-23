a4fe90ba4867686d4fa801aee97e3d2411965217
sam <tonnymareba217@gmail.com>
2025-11-23 22:52:52 +0000

    bak1

--- index.js
+++ index.js
@@ -23,5 +23,6 @@ app.use("/auth", authRoutes);
 const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port`);
+    console.log(PORT);
 });
