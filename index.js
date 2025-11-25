2a0a1fc0009798aaf573052949e6fa2e6fc05d09
sam <tonnymareba217@gmail.com>
2025-11-25 08:36:15 +0000

    bak1

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import cookieParser from "cookie-parser";
-import connectDB from "./config/db.js";
-import authRoutes from "./router/auth.js";
+import { connectDB } from "./config/db.js";
+import authRoutes from "./Router/auth.js";
 
 dotenv.config();
 
-
 const app = express();
 
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
 app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-}));
 
-connectDB();
 app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+
+
+app.get("/", (req, res) => {
+    res.send("API running");
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port`);
-    console.log(PORT);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
